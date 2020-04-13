be14f3a52c81bc973bb99e91eda4d4a77d16dae5
Aditya Sharad <adityasharad@github.com>
2020-04-12 21:29:38 -0700

    Teach CLI to read input params from a file
    Input data must be a Scenario object.

--- src/algorithms/cli.ts
+++ src/algorithms/cli.ts
@@ -6,6 +6,7 @@ import { SeverityTableRow } from '../components/Main/Scenario/ScenarioTypes'
 import { TimeSeries } from './types/TimeSeries.types'
 import { getScenarioData } from '../components/Main/state/scenarioData'
 import { getCountryAgeDistribution } from '../components/Main/state/countryAgeDistributionData'
+import { Scenario } from '../.generated/types'
 
 const handleRejection: NodeJS.UnhandledRejectionListener = err => {
   throw err
@@ -15,10 +16,9 @@ process.on('unhandledRejection', handleRejection);
 
 enum Args {
   // 0 is node, 1 is this script
-  // Params,
+  Params = 2
   // Severity,
   // CountryAgeDistribution,
-  Containment = 2
 }
 
 function readJsonFromFile<T>(args: Args) {
@@ -37,25 +37,14 @@ function readJsonFromFile<T>(args: Args) {
 }
 
 async function main() {
-  const region = process.argv[2];
-  if (!region) {
-    usage();
-  }
-  console.log(`Running model for region ${region}`);
+  const scenario = readJsonFromFile<Scenario>(Args.Params);
   const outputFile = process.argv[3];
   if (!outputFile) {
     usage();
   }
   console.log(`Writing output to ${outputFile}`);
-  
-  // This reads directly from scenarios.json and validates the JSON.
-  let params: AllParams;
-  try {
-    params = getScenarioData(region);
-  } catch(e) {
-    console.error(e);
-    process.exit(1);
-  }
+
+  const params = scenario.allParams;
   const paramsFlat: AllParamsFlat = {
     ...params.population,
     ...params.epidemiological,
@@ -65,9 +54,8 @@ async function main() {
   // This reads directly from severityData.json.
   const severity = severityData as SeverityTableRow[];
   // This reads directly from country_age_distribution.json and validates the JSON.
-  const ageDistribution = getCountryAgeDistribution(region);
+  const ageDistribution = getCountryAgeDistribution(params.population.country);
   // This reads containment info from the scenario.
-  // TODO make this customisable, the user will expect to change the values.
   const containment = intervalsToTimeSeries(params.containment.mitigationIntervals);
   const result = await run(paramsFlat, severity, ageDistribution, containment);
   console.log('Run complete');
@@ -80,7 +68,7 @@ function usage() {
     `
 Usage:
 
-    cli <country> <output-file>
+    cli <input-file> <output-file>
 
         Manually perform a single model run with the given input. Print the output to stdout.
 
